b6063bb6e439c73e6870615a85220ec97f500510
Ashton Baker <ashtonsb@umich.edu>
2017-10-20 15:29:41 -0400

    STILL BROKE AFTER ALL THESE YEARS

--- training/master.py
+++ training/master.py
@@ -1,5 +1,6 @@
 
 from google.cloud import pubsub_v1
+from google.cloud import storage
 
 def create_topic(project, topic_name):
     """Create a new Pub/Sub topic."""
@@ -9,6 +10,40 @@ def create_topic(project, topic_name):
     topic = publisher.create_topic(topic_path)
 
     print('Topic created: {}'.format(topic))
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    blob.delete()
+
+    print('Blob {} deleted.'.format(blob_name))
 
 def create_player_database(num_players):
     return None
